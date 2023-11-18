037656a1c7d9c051d0bd8eed6b4ab837841c74c7
Julian Puyo <112900679+puyojulian@users.noreply.github.com>
2023-11-17 21:46:19 -0500

    avances sobre reconstruirCadenaMejorado y reconstruirCadenaTurbo, probadas hasta secuencias de 128 caracteres

--- src/main/scala/ReconstCadenas/package.scala
+++ src/main/scala/ReconstCadenas/package.scala
@@ -2,35 +2,64 @@ import ArbolSufijos._
 import Oraculo._
 
 package object ReconstCadenas {
-  def reconstruirCadenaIngenuo (n : Int , o : Oraculo ) : Seq[Char]= {
+  def reconstruirCadenaIngenuo(n: Int, o: Oraculo): Seq[Char] = {
     // recibe la longitud de la secuencia que hay que reconstruir (n), y un oraculo para esa secuencia
     // y devuelve la secuencia reconstruida
     val alfabeto = Seq('a', 'c', 'g', 't')
-
     def generarSecuencias(n: Int, alphabet: Seq[Char]): Set[Seq[Char]] = {
       val initialSet: Set[Seq[Char]] = Set(Seq.empty[Char])
       (1 to n).foldLeft(initialSet) { (acc, _) =>
         acc.flatMap(seq => alphabet.map(char => seq :+ char))
       }
     }
-
     val secuencias = generarSecuencias(n, alfabeto)
-
     val resultado = secuencias.to(LazyList).find(o)
     resultado.getOrElse(Seq.empty[Char])
   }
-//  def reconstruirCadenaMejorado (n : Int , o : Oraculo ) : Seq [Char]= {
-//    // recibela longitud de la secuencia que hay que reconstruir (n), y un oraculo para esa secuencia
-//    // y devuelve la secuencia reconstruida
-//    // Usa la propiedad de que si s=s1++s2 entonces s1 y s2 tambien son subsecuencias de s
-//
-//  }
-//  def reconstruirCadenaTurbo (n : Int , o : Oraculo ) : Seq [Char]= {
-//    // recibela longitud de la secuencia que hay que reconstruir (n, potencia de 2), y un oraculo para esa secuencia
-//    // y devuelve la secuencia reconstruida
-//    // Usa la propiedad de que si s=s1++s2 entonces s1 y s2 tambien son subsecuencias de s
-//
-//  }
+  def reconstruirCadenaMejorado(n: Int, o: Oraculo ): Seq[Char] = {
+    // recibela longitud de la secuencia que hay que reconstruir (n), y un oraculo para esa secuencia
+    // y devuelve la secuencia reconstruida
+    // Usa la propiedad de que si s=s1++s2 entonces s1 y s2 tambien son subsecuencias de s
+    val alfabeto = Seq('a', 'c', 'g', 't')
+    def generarCadena(k: Int, SC:Set[Seq[Char]]): Seq[Char] = {
+      val conjSec = SC.flatMap(seq => alfabeto.map(char => seq :+ char))
+      val newSC = for {
+        w <- conjSec
+        if o(w)
+      } yield {
+        w
+      }
+      val resultado = newSC.to(LazyList).find(_.length == n)
+      resultado match {
+        case Some(seq) => seq
+        case None =>
+          if (k > n) Seq.empty[Char]
+          else generarCadena(k + 1, newSC)
+      }
+    }
+    generarCadena(1, Set(Seq.empty[Char]))
+  }
+
+  def reconstruirCadenaTurbo(n : Int , o : Oraculo ): Seq [Char] = {
+    // recibela longitud de la secuencia que hay que reconstruir (n, potencia de 2), y un oraculo para esa secuencia
+    // y devuelve la secuencia reconstruida
+    // Usa la propiedad de que si s=s1++s2 entonces s1 y s2 tambien son subsecuencias de s
+    val alfabeto = Seq('a', 'c', 'g', 't')
+
+    def generarCadenaTurbo(k: Int, SC: Set[Seq[Char]]): Seq[Char] = {
+      val conjSec = SC.flatMap(seq1 => SC.map(seq2 => seq1 ++ seq2))
+      val newSC = conjSec.filter(o)
+      val resultado = newSC.to(LazyList).find(_.length == n)
+      resultado match {
+        case Some(seq) => seq
+        case None =>
+          if (k > n) Seq.empty[Char]
+          else generarCadenaTurbo(k*2, newSC)
+      }
+    }
+    val conjuntoInicial: Set[Seq[Char]] = alfabeto.map(Seq(_)).toSet
+    generarCadenaTurbo(2, conjuntoInicial)
+  }
 //  def reconstruirCadenaTurboMejorada (n : Int , o : Oraculo ) : Seq [Char]= {
 //    // recibela longitud de la secuencia que hay que reconstruir (n, potencia de 2), y un oraculo para esa secuencia
 //    // y devuelve la secuencia reconstruida
