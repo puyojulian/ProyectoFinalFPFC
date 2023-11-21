435ca03e8a80851a51c95fb7aada559792901c69
Jpdv9 <jean.davalos@correounivalle.edu.co>
2023-11-21 16:18:59 -0500

    Subiendo la funcion Turbo Mejorada

--- src/main/scala/ReconstCadenas/package.scala
+++ src/main/scala/ReconstCadenas/package.scala
@@ -60,13 +60,45 @@ package object ReconstCadenas {
     val conjuntoInicial: Set[Seq[Char]] = alfabeto.map(Seq(_)).toSet
     generarCadenaTurbo(2, conjuntoInicial)
   }
-//  def reconstruirCadenaTurboMejorada (n : Int , o : Oraculo ) : Seq [Char]= {
-//    // recibela longitud de la secuencia que hay que reconstruir (n, potencia de 2), y un oraculo para esa secuencia
-//    // y devuelve la secuencia reconstruida
-//    // Usa la propiedad de que si s=s1++s2 entonces s1 y s2 tambien son subsecuencias de s
-//    // Usa el filtro para ir mas rapido
-//
-//  }
+  def reconstruirCadenaTurboMejorada (n : Int , o : Oraculo ) : Seq [Char]= {
+    // recibela longitud de la secuencia que hay que reconstruir (n, potencia de 2), y un oraculo para esa secuencia
+    // y devuelve la secuencia reconstruida
+    // Usa la propiedad de que si s=s1++s2 entonces s1 y s2 tambien son subsecuencias de s
+    // Usa el filtro para ir mas rapido
+
+    // Definición del alfabeto
+    val alfabeto = Seq('a', 'c', 'g', 't')
+
+    // Función principal para generar la cadena turbo mejorada
+    def generarCadenaTurbo(k: Int, SC: Set[Seq[Char]]): Seq[Char] = {
+      val conjSec = SC.flatMap(seq1 => SC.map(seq2 => seq1 ++ seq2))
+      val newSC = conjSec.filter(s => o(s))
+
+      val resultado = newSC.to(LazyList).find(_.length == n)
+      resultado match {
+        case Some(seq) => seq
+        case None =>
+          if (k > n) Seq.empty[Char]
+          else generarCadenaTurbo(k * 2, filtrar(newSC, k))
+      }
+    }
+
+    // Función de filtrado para eliminar secuencias problemáticas según la descripción dada
+    def filtrar(SC: Set[Seq[Char]], k: Int): Set[Seq[Char]] = {
+      SC.filter { s1 =>
+        SC.forall { s2 =>
+          val s = s1 ++ s2
+          SC.exists(w => w.sliding(k, 1).forall(sub => s.containsSlice(sub)))
+        }
+      }
+    }
+
+    // Conjunto inicial de secuencias de longitud 1 del alfabeto
+    val conjuntoInicial: Set[Seq[Char]] = alfabeto.map(Seq(_)).toSet
+
+    // Llamada inicial a la función de generación
+    generarCadenaTurbo(2, conjuntoInicial)
+  }
 //  def reconstruirCadenaTurboAcelerada (n : Int , o : Oraculo ) : Seq [Char]= {
 //    // recibela longitud de la secuencia que hay que reconstruir (n , potencia de 2), y un oraculo para esa secuencia
 //    // y devuelve la secuencia reconstruida
--- src/main/scala/ReconstCadenasPar/package.scala
+++ src/main/scala/ReconstCadenasPar/package.scala
@@ -2,15 +2,47 @@ import common._
 import scala.collection.parallel.CollectionConverters._
 import Oraculo._
 import ArbolSufijos._
+import scala.concurrent.{Future, Await}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 package object ReconstCadenasPar {
   // Ahora versiones paralelas
-//  def reconstruirCadenaIngenuoPar ( umbral : Int ) (n : Int , o : Oraculo ) : Seq [Char]= {
-//    // recibe la longitud de la secuencia que hay que reconstruir (n), y un oraculo para esa secuencia
-//    // y devuelve la secuencia reconstruida
-//    // Usa paralelismo de tareas
-//
-//  }
+  /*def reconstruirCadenaIngenuoPar ( umbral : Int ) (n : Int , o : Oraculo ) : Seq [Char]= {
+    // recibe la longitud de la secuencia que hay que reconstruir (n), y un oraculo para esa secuencia
+    // y devuelve la secuencia reconstruida
+    // Usa paralelismo de tareas
+
+    val alfabeto = Seq('a', 'c', 'g', 't')
+
+    def generarSecuencias(n: Int, alphabet: Seq[Char]): Set[Seq[Char]] = {
+      val initialSet: Set[Seq[Char]] = Set(Seq.empty[Char])
+      (1 to n).foldLeft(initialSet) { (acc, _) =>
+        acc.flatMap(seq => alphabet.map(char => seq :+ char))
+      }
+    }
+
+    val secuencias = generarSecuencias(n, alfabeto)
+
+    // Función que verifica el oráculo en paralelo
+    def checkOraculoParallel(seq: Seq[Char]): Future[Option[Seq[Char]]] =
+      Future {
+        if (o(seq)) Some(seq) else None
+      }
+
+    // Convertir secuencias a lista de futuros Future
+    val futures: List[Future[Option[Seq[Char]]]] = secuencias.toList.map(checkOraculoParallel)
+
+    // Ejecutar futuros en paralelo con umbral de concurrencia
+    val parallelFutures: Future[Option[Seq[Char]]] = Future.sequence(futures).map(_.find(_.isDefined).flatten)
+
+    // Obtener el resultado sincrónicamente
+    val resultado: Option[Seq[Char]] = Await.result(parallelFutures, Duration.Inf)
+
+    resultado.getOrElse(Seq.empty[Char])
+
+
+  }*/
 //  def reconstruirCadenaMejoradoPar ( umbral : Int ) (n : Int , o : Oraculo ) : Seq [Char]= {
 //    // recibe la longitud de la secuencia que hay que reconstruir (n), y un oraculo para esa secuencia
 //    // y devuelve la secuencia reconstruida
@@ -39,5 +71,5 @@ package object ReconstCadenasPar {
 //    // Usa arboles de sufijos para guardar Seq[Seq[Char]]
 //    // Usa paralelismo de tareas y/o datos
 //
-//  }
+//  }*/
 }
